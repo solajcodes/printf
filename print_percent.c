f9fc1a53c372b202d1af3daaa591aa01c9809739
solajcodes <103960554+solajcodes@users.noreply.github.com>
2022-11-20 08:57:19 +0100

    Update and rename utils.c to print_percent.c

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_percent - prints a % character
+ * @args: arguments (unused)
+ *
+ * Return: 1
+ */
+int print_percent(va_list __attribute__((unused)) args)
+{
+	_putchar('%');
+
+	return (1);
+}
